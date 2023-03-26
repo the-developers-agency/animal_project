a737b12c5d73c16126b5c0d6afbe549e62565f4a
Mohan Raj <mohanzolomon@gmail.com>
2023-03-26 11:05:56 +0530

    make set up

--- App.js
+++ App.js
@@ -2,25 +2,35 @@ import React, {useEffect, useState} from 'react';
 import {NativeBaseProvider} from 'native-base';
 import {NavigationContainer} from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {doc, setDoc} from 'firebase/firestore';
+// import {doc, setDoc} from 'firebase/firestore';
 import 'react-native-get-random-values';
 import {v4 as uuidv4} from 'uuid';
 import RootStack from './src/navigation/RootStack';
 import {LanguageContext} from './src/Contexts/LanguageContexts';
 import {db} from './src/utils/firebase';
+import firebase from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
 
 const App = () => {
   const [language, setLanguage] = useState('eng');
 
   useEffect(() => {
     (async () => {
+      // firebase.initializeApp();
       try {
         const userExist = await AsyncStorage.getItem('user');
         if (!userExist) {
+          console.log("User doesn't exist");
           const id = uuidv4();
-          await setDoc(doc(db, 'users', id), {});
-          AsyncStorage.setItem('user', JSON.stringify({id}));
+          await AsyncStorage.setItem('user', JSON.stringify({id}));
+          const usersCollection = firestore().collection('Users');
+          await usersCollection.doc(id).set({
+            id,
+          });
+        } else {
+          console.log("User does exist");
         }
+
       } catch (err) {
         console.log('ERROR:: ', err);
       }
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,12 +2,13 @@ import {initializeApp, getApps} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA7WQQWvM-3DxS7mJBsJmNrJ9FHiIJT0Es',
-  authDomain: 'animal-app-61d43.firebaseapp.com',
-  projectId: 'animal-app-61d43',
-  storageBucket: 'animal-app-61d43.appspot.com',
-  messagingSenderId: '336611831235',
-  appId: '1:336611831235:web:020e5a352ed5fa104aa176',
+  apiKey: 'AIzaSyCulWCf_LtuKMDXXzU7h83CnBwEenrn_n4',
+  authDomain: 'animal-app-tda.firebaseapp.com',
+  projectId: 'animal-app-tda',
+  storageBucket: 'animal-app-tda.appspot.com',
+  messagingSenderId: '150425401694',
+  appId: '1:150425401694:web:9c6d33d31df008d80377cf',
+  measurementId: 'G-VNKH0G1SWM',
 };
 
 const app = initializeApp(firebaseConfig);
